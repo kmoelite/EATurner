87c5dbea9bce909752cb9c3fbb58709c79331f60
Mohan, Kevin <Kevin.Mohan@aarons.com>
2017-04-23 12:54:01 -0400

    Adding Entity Framework datamodel

---
+++ EATurnerDataService/Scripts/indexScript.js
@@ -0,0 +1,15 @@
+﻿function searchByActor() {
+    $('#criteriaButton').html('<b style="color:red">Actor Search</b>');
+}
+
+function searchByTitle() {
+    $('#criteriaButton').html('<b style="color:red">Title Search</b>');
+}
+
+function searchByGenre() {
+    $('#criteriaButton').html('<b style="color:red">Genre Search</b>');
+}
+
+function searchByAward() {
+    $('#criteriaButton').html('<b style="color:red">Award Search</b>');
+}
